a7366b1fcebe4d1c72e31b066cf3c21bcf8b9800
Yann Langue <Yann.Langue@clarksons.com>
2020-02-20 18:29:30 +0000

    stuff.

---
+++ src/fixtures/index.js
@@ -0,0 +1,19 @@
+export const singularStory = {
+	by: 'Jon Leonard',
+	id: 1,
+	time: 1567209822,
+	title:'Tarnished Google Responds',
+	url: 'https://something.com/hackernewstut',
+}
+
+export const storyIds = [1];
+
+
+
+export const singularStory = {
+	by: undefined,
+	id: undefined,
+	time: undefined,
+	title: undefined,
+	url: undefined,
+}
